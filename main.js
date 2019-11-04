95229bd085121e562afd9721a5baba8fcdcc20a8
Nicholas Bratton <47389141+nick-bratton@users.noreply.github.com>
2019-11-04 13:47:23 +0100

    trivial: fixes tab and indent formatting

--- main.js
+++ main.js
@@ -1,69 +1,57 @@
-const { selection } = require("scenegraph")
-const { Artboard, Rectangle, Ellipse, Text } = require("scenegraph");
 let panel;
 
 function create() {
 	const HTML =
-			`<style>
-					.break {
-							flex-wrap: wrap;
-					}
-					label.row > span {
-							color: #8E8E8E;
-							width: 20px;
-							text-align: right;
-							font-size: 9px;
-					}
-					label.row input {
-							flex: 1 1 auto;
-					}
-					.show {
-							display: block;
-					}
-					.hide {
-							display: none;
-					}
-			</style>
-			<form method="dialog" id="main">
-					<div class="row break">
-							<label class="row">
-									<span>↕︎</span>
-									<input type="number" uxp-quiet="true" id="inputScaleFactor" placeholder="Scale Factor (e.g., 2)" />
-							</label>
-					</div>
-					<footer><button id="ok" type="submit" uxp-variant="cta">Apply</button></footer>
-			</form>
-			<p id="warning">Select something to scale or CMD+A to select everything.</p>
-			`
+		`<style>
+				.break {
+						flex-wrap: wrap;
+				}
+				label.row > span {
+						color: #8E8E8E;
+						width: 20px;
+						text-align: right;
+						font-size: 9px;
+				}
+				label.row input {
+						flex: 1 1 auto;
+				}
+				.show {
+						display: block;
+				}
+				.hide {
+						display: none;
+				}
+		</style>
+		<form method="dialog" id="main">
+				<div class="row break">
+						<label class="row">
+								<span>↕︎</span>
+								<input type="number" uxp-quiet="true" id="inputScaleFactor" placeholder="Scale Factor (e.g., 2)" />
+						</label>
+				</div>
+				<footer><button id="ok" type="submit" uxp-variant="cta">Apply</button></footer>
+		</form>
+		<p id="warning">Select something to scale or CMD+A to select everything.</p>
+		`
 	function scale() {
-			const { editDocument } = require("application");
-			const scaleFactor = Number(document.querySelector("#inputScaleFactor").value);
-
-			editDocument({ editLabel: "Scale selection" }, function (selection) {
-					
-					// 1. need to handle locked items
-					// console.log(selection.itemsIncludingLocked.length);
-
-					// 2. need to reposition and scale probably  
-
-					// 3. need to handle shadows, strokes, blur, background blur, vectors
-
-
-					if (selection.items[0].constructor.name == 'Artboard'){
-						// let artboard = selection.items[0];
-						// artboard.resize(artboard.localBounds.width *= scaleFactor, artboard.localBounds.height *= scaleFactor);
-						scaleArtboardAndItsChildren(selection, scaleFactor);
-					}
-					else {
-						scaleSelection(selection, scaleFactor);
-					}
-			})
+		const { editDocument } = require("application");
+		const scaleFactor = Number(document.querySelector("#inputScaleFactor").value);
+		editDocument({ editLabel: "Scale selection" }, function (selection) {
+			// 1. need to handle locked items
+			// console.log(selection.itemsIncludingLocked.length);
+			// 2. need to reposition and scale probably  
+			// 3. need to handle shadows, strokes, blur, background blur, vectors
+			if (selection.items[0].constructor.name == 'Artboard'){
+				scaleArtboardAndItsChildren(selection, scaleFactor);
+			}
+			else {
+				scaleSelection(selection, scaleFactor);
+			}
+		})
 	}
-
 	panel = document.createElement("div");
 	panel.innerHTML = HTML;
 	panel.querySelector("form").addEventListener("submit", scale);
-
 	return panel;
 }
 
@@ -86,26 +74,26 @@ function applyModifications (node, scaleFactor){
 			translateByScaleFactor(node, scaleFactor);
 			break;
 		case 'Ellipse':
-				node.radiusX *= scaleFactor;
-				node.radiusY *= scaleFactor;
-				translateByScaleFactor(node, scaleFactor);
+			node.radiusX *= scaleFactor;
+			node.radiusY *= scaleFactor;
+			translateByScaleFactor(node, scaleFactor);
 			break;
 		case 'Line':
-				node.resize(node.localBounds.width *= scaleFactor, node.localBounds.height *= scaleFactor);
-				translateByScaleFactor(node, scaleFactor);
+			node.resize(node.localBounds.width *= scaleFactor, node.localBounds.height *= scaleFactor);
+			translateByScaleFactor(node, scaleFactor);
 			break;
 		case 'Text':
-				node.fontSize *= scaleFactor;
-				node.lineSpacing *= scaleFactor;
-				translateByScaleFactor(node, scaleFactor);
+			node.fontSize *= scaleFactor;
+			node.lineSpacing *= scaleFactor;
+			translateByScaleFactor(node, scaleFactor);
 			break;
 		case 'Group':
-				node.resize(node.localBounds.width *= scaleFactor, node.localBounds.height *= scaleFactor);
-				translateByScaleFactor(node, scaleFactor);
+			node.resize(node.localBounds.width *= scaleFactor, node.localBounds.height *= scaleFactor);
+			translateByScaleFactor(node, scaleFactor);
 			break;
 		case 'SymbolInstance':
-				node.resize(node.localBounds.width *= scaleFactor, node.localBounds.height *= scaleFactor);
-				translateByScaleFactor(node, scaleFactor);
+			node.resize(node.localBounds.width *= scaleFactor, node.localBounds.height *= scaleFactor);
+			translateByScaleFactor(node, scaleFactor);
 			break;
 		default: 
 			console.log('This plugin does not know how to handle layers of type: ' + node.constructor.name);
@@ -127,13 +115,13 @@ function scaleArtboardAndItsChildren(selection, scaleFactor){
 }
 
 function show(event) {
-		if (!panel) event.node.appendChild(create());
+	if (!panel) event.node.appendChild(create());
 }
 
 module.exports = {
-		panels: {
-				scaleEverything: {
-						show,
-				}
+	panels: {
+		scaleEverything: {
+			show,
 		}
+	}
 };
